9eae829a32169dd483778f5e1d47298cbd094c93
paf <paf@titelfrei.de>
2021-04-19 11:28:57 +0200

    tries to fix the unit tests on CI by adding no-sandbox

---
+++ src/gitlab-ci.karma.conf.js
@@ -0,0 +1,10 @@
+const config = require('./karma.conf');
+
+config.customLaunchers.ChromeHeadlessNoSandbox = {
+  base: 'ChromeHeadless',
+  flags: ['--no-sandbox']
+};
+
+config.browsers = ['ChromeHeadlessNoSandbox'];
+
+exports.config = config;
